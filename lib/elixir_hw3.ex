ec0c7097e8befb87e6480033665cbc87c9e4cd0c
Rim-777 <javatimomoss@gmail.com>
2021-12-19 16:32:46 +0100

    add hw3

--- lib/elixir_hw3.ex
+++ lib/elixir_hw3.ex
@@ -1,21 +1,67 @@
 defmodule ElixirHW3 do
   @moduledoc false
 
-  def word_count do
-    {:ok, text} = File.read("lib/files/some_text.txt")
-    String.replace(text, ~r/[\p{P}\p{S}]/, "")
-    |> String.split(" ", trim: true)
-    |> Enum.reduce(
-         0,
-         fn word, acc ->
-           acc = acc + 1
-         end
-       )
+  def word_count(file_path) do
+    {:ok, text} = File.read(file_path)
+    count_from(text)
   end
 
-  def parallelized_word_count do
-   "lib/files/some_text.txt"
-   |> File.stream!(read_ahead: 100_000)
-   |> Flow.from_enumerable()
+  def word_count_by_lines(file_path) do
+    streamed_file!(file_path)
+    |> Enum.reduce(
+         %{total: 0},
+         fn line, acc ->
+           Map.update!(acc, :total, & &1 + count_from(line))
+         end
+       )
+    |> Map.get(:total)
+  end
+
+  def parallelized_word_count(file_path) do
+    streamed_file!(file_path)
+    |> Flow.from_enumerable()
+    |> handle_and_result_flow
+  end
+
+  def count_from_files(dir_path) do
+    for file <- File.ls!(dir_path) do
+      streamed_file!("#{dir_path}/#{file}")
+    end
+
+    |> Flow.from_enumerables()
+    |> handle_and_result_flow
+  end
+
+  defp count_from(text) do
+    prepared_text(text)
+    |> String.split(" ", trim: true)
+    |> Enum.count
+  end
+
+  defp prepared_text(text) do
+    String.replace(text, ~r/[!#$%&()*+,.:;\r\n<=>?@\^_`{|}~-]/, " ")
+  end
+
+  defp result_flow(flow) do
+    Enum.reduce(
+      flow,
+      0,
+      &elem(&1, 1) + &2
+    )
+  end
+
+  defp streamed_file!(file_path) do
+    File.stream!(file_path, read_ahead: 100_000)
+  end
+
+  defp handle_and_result_flow(flow) do
+    Flow.partition(flow)
+    |> Flow.reduce(
+         fn -> %{total: 0} end,
+         fn line, acc ->
+           Map.update!(acc, :total, & &1 + count_from(line))
+         end
+       )
+    |> result_flow
   end
 end
--- test/elixir_hw3_test.exs
+++ test/elixir_hw3_test.exs
@@ -1,4 +1,28 @@
 defmodule ElixirHW3Test do
   use ExUnit.Case
   doctest ElixirHW3
+
+  describe "word_count/1" do
+    test "count number of words from a file" do
+      assert ElixirHW3.word_count("test/files/some_text.txt") == 320
+    end
+  end
+
+  describe "word_count_by_lines/1" do
+    test "count number of words from a file by lines" do
+      assert ElixirHW3.word_count_by_lines("test/files/some_text.txt") == 320
+    end
+  end
+
+  describe "parallelized_word_count/1" do
+    test "count number of words from a file by lines" do
+      assert ElixirHW3.parallelized_word_count("test/files/some_text.txt") == 320
+    end
+  end
+
+  describe "count_from_files/1" do
+    test "count number of words from a file by lines" do
+      assert ElixirHW3.count_from_files("test/files") == 640
+    end
+  end
 end
